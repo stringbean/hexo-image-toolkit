0f06ae4078531737ef3609da4d7f92881f0e344a
Michael Stringer <stringbean@users.noreply.github.com>
2020-09-10 19:55:03 +0100

    Prettier

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const postImg = require('./lib/post_img');
 
 hexo.extend.tag.register(
   'post_img',
-  function(args) {
+  function (args) {
     return postImg(hexo, args, this);
   },
   { async: true },
